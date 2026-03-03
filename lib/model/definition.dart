f302690fa37deaf63221acbbea3f175da128d904
KTH <pni2396@gmail.com>
2026-03-03 22:53:03 +0900

    refactor: 불필요한 DTO 수정

--- lib/model/definition.dart
+++ lib/model/definition.dart
@@ -1,38 +1,11 @@
 import 'package:mobidic_flutter/type/part_of_speech.dart';
 
 class Definition {
-  String id;
-  String wordId;
-  String definition;
-  PartOfSpeech part;
-
-  Definition({
-    required this.id,
-    required this.wordId,
-    required this.definition,
-    required this.part,
-  });
-
-  factory Definition.fromDto(DefDto dto) => Definition(
-    id: dto.id,
-    wordId: dto.wordId,
-    definition: dto.definition,
-    part: dto.part,
-  );
-}
-
-class DefDto {
   final String id;
-  final String wordId;
   final String definition;
   final PartOfSpeech part;
 
-  DefDto({
-    required this.id,
-    required this.wordId,
-    required this.definition,
-    required this.part,
-  });
+  Definition({required this.id, required this.definition, required this.part});
 
   static PartOfSpeech parsePart(String value) {
     return PartOfSpeech.values.firstWhere(
@@ -41,17 +14,9 @@ class DefDto {
     );
   }
 
-  factory DefDto.fromJson(Map<String, dynamic> json) => DefDto(
-    id: json['id'] ?? (throw Exception("id is null")),
-    wordId: json['wordId'] ?? (throw Exception("wordId is null")),
-    definition: json['definition'] ?? (throw Exception("definition is null")),
+  factory Definition.fromJson(Map<String, dynamic> json) => Definition(
+    id: json['id'],
+    definition: json['definition'],
     part: parsePart(json['part']),
   );
-}
-
-class DefWithPart {
-  final String definition;
-  final PartOfSpeech part;
-
-  DefWithPart({required this.definition, required this.part});
 }
