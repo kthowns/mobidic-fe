a144115629929ffa1810f683b641fadf0ab63554
KTH <pni2396@gmail.com>
2026-03-13 21:49:01 +0900

    fix: 로그아웃 처리 로직 수정

--- lib/view/auth/auth_guard.dart
+++ lib/view/auth/auth_guard.dart
@@ -10,14 +10,13 @@ class AuthGuard extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     ref.listen<AuthState>(authViewModelProvider, (previous, next) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text('로그아웃 되었습니다.'),
-          duration: Duration(seconds: 2),
-        ),
-      );
-
       if (previous?.currentUser != null && next.currentUser == null) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('로그아웃 되었습니다.'),
+            duration: Duration(seconds: 2),
+          ),
+        );
         context.go('/'); // 로그아웃 되면 로그인 페이지로 강제 이동
       }
     });
