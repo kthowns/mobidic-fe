e869dd3ef48457862de19e8dcb95a5a599ff5134
KTH <pni2396@gmail.com>
2026-04-28 04:55:49 +0900

    build: 안드로이드 빌드 옵션 세팅
    - 패키지명 수정
    - Kotlin 버전 다운그레이드 (2.0.21 -> 1.9.24)
    - 서명키 설정 추가
    - 빌드 옵션 incremental=false 설정

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,10 +8,30 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
 android {
-    namespace = "com.kimtaeyang.mobidic_flutter"
+    namespace = "com.kthowns.mobidic"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
+
+    signingConfigs {
+        create("release") {// getProperty를 사용하는 것이 안전합니다.
+            keyAlias = keystoreProperties.getProperty("keyAlias")
+            keyPassword = keystoreProperties.getProperty("keyPassword")
+            storePassword = keystoreProperties.getProperty("storePassword")
+
+            val stFile = keystoreProperties.getProperty("storeFile")
+            if (stFile != null) {
+                // android/ 폴더 기준이므로 rootProject.file을 사용해 경로를 잡습니다.
+                storeFile = rootProject.file(stFile)
+            }
+        }
+    }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -24,7 +44,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.kimtaeyang.mobidic_flutter"
+        applicationId = "com.kthowns.mobidic"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 24
@@ -44,7 +64,11 @@ android {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
+
+            isMinifyEnabled = true  // 코드 난독화 (권장)
+            isShrinkResources = true // 미사용 리소스 제거 (권장)
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
 }
--- android/app/src/main/java/com/kimtaeyang/mobidic_flutter/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.kimtaeyang.mobidic_flutter;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
